1024c3256c9838f246bcfcc1d2de98924e9b214b
durgasankar <durgasankar.raja500@gmail.com>
2020-02-24 14:47:39 +0530

    [durgasankar].add-> fundoo project created with flex and angular material added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from "./containers/page-not-found/page-not-found.component";
+import { LoginComponent } from "./containers/user-authentication/login/login.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { RegistrationComponent } from "./containers/user-authentication/registration/registration.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/login" },
+  { path: "register", component: RegistrationComponent },
+  { path: "login", component: LoginComponent },
+  { path: "**", component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import {
+  MatButtonModule,
+  MatInputModule,
+  MatCardModule,
+  MatFormFieldModule
+} from "@angular/material";
+import { PageNotFoundComponent } from "./containers/page-not-found/page-not-found.component";
+import { LoginComponent } from './containers/user-authentication/login/login.component';
+import { RegistrationComponent } from './containers/user-authentication/registration/registration.component';
 
+const MaterialComponents = [
+  MatButtonModule,
+  MatInputModule,
+  MatCardModule,
+  MatFormFieldModule
+];
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, PageNotFoundComponent, LoginComponent, RegistrationComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MaterialComponents
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
