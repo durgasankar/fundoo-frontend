405c57fba23030be7ddf9e32c71d7c539641b0a3
durgasankar <durgasankar.raja500@gmail.com>
2020-02-27 16:15:27 +0530

    [durgasankar].add-> login validations added

--- src/app/containers/user-authentication/login/login.component.ts
+++ src/app/containers/user-authentication/login/login.component.ts
@@ -1,4 +1,5 @@
-import { FormGroup } from "@angular/forms";
+import { UserService } from "src/app/services/user.service";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { LoginUser } from "./../../../models/login-user";
 import { Component, OnInit } from "@angular/core";
 
@@ -8,7 +9,29 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  loginDto: LoginUser = new LoginUser();
+  loginForm: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private _userService: UserService
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      emailId: [
+        null,
+        Validators.compose([
+          Validators.compose([Validators.required, Validators.email])
+        ])
+      ],
+      password: [
+        null,
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(10)
+        ])
+      ]
+    });
+  }
 }
